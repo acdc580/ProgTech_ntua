21919751a6038034fdb8dc75d154b63222f1ed46
acdc580 <acdc580-github@email.com>
2026-06-25 18:57:19 +0300

    basic structure

---
+++ worksheet-1/babyratio.cpp
@@ -0,0 +1,33 @@
+#ifndef CONTEST
+#include "babyratio.hpp"
+#endif
+
+using namespace std;
+
+rational::rational(int n, int d) { 
+
+};
+
+rational rational::add(rational r) { 
+
+};
+
+rational rational::sub(rational r) { 
+
+};
+
+rational rational::mul(rational r) { 
+
+};
+
+rational rational::div(rational r) { 
+
+};
+
+void rational::print() { 
+
+};
+
+int rational::gcd(int a, int b) { 
+
+};
